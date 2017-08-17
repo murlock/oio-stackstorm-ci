a5f786c13f0ab87e35be537382d0d016ed118327
Michael Bonfils <murlock42@gmail.com>
2017-08-17 17:42:56 +0200

    run_job is now a class
    Before, it was not very friendly to implement a garbage
    collector to destroy VM when something bad occurs.
    
    Now, there is cleanup method AND it is also called by destructor

--- actions/everything.py
+++ actions/everything.py
@@ -6,6 +6,7 @@ import os
 import tempfile
 import time
 import shutil
+import traceback
 import uuid
 
 try:
@@ -23,57 +24,70 @@ from run_vm import do_run
 from html import create_html_report
 
 
-def run_job():
-    # FIXME: should be PR name or git revision
-    create_vm.SERVER_NAME = "{0}-{1}".format("master", str(uuid.uuid1()))
-    create_vm.VOLUME_NAME = create_vm.SERVER_NAME + "-vol"
-    create_vm.KEYPAIR_NAME = create_vm.SERVER_NAME + "-id"
+class OpenstackJob(object):
+    def __init__(self):
+        self.properties = {}
 
-    print("Create Instance")
-    properties = create_vm.create_server(create_vm.CONN)
+    def run(self):
+        # FIXME: should be PR name or git revision
+        create_vm.SERVER_NAME = "{0}-{1}".format("master", str(uuid.uuid1()))
+        create_vm.VOLUME_NAME = create_vm.SERVER_NAME + "-vol"
+        create_vm.KEYPAIR_NAME = create_vm.SERVER_NAME + "-id"
 
-    print("key priv\n", properties['keypriv'])
-    print("key pub\n", properties['keypub'])
+        print("Create Instance")
+        self.properties = create_vm.create_server(create_vm.CONN)
 
-    # TODO: ensure that Instance is started and available
-    time.sleep(20)
+        print("key priv\n", self.properties['keypriv'])
+        print("key pub\n", self.properties['keypub'])
 
-    print("Prepare and build Docker template")
-    do_prepare(properties['ip'],
+        # ssh_connect use retry mecanism to connect to VM
+        # but we still use a delay to allow VM to boot
+        time.sleep(20)
+
+        print("Prepare and build Docker template")
+        do_prepare(self.properties['ip'],
+                   'ubuntu',
+                   self.properties['keypriv'])
+
+        print("Launch test")
+        do_run(self.properties['ip'],
                'ubuntu',
-               properties['keypriv'])
+               self.properties['keypriv'])
 
-    print("Launch test")
-    do_run(properties['ip'],
-           'ubuntu',
-           properties['keypriv'])
+        tmpdir = tempfile.mkdtemp()
 
-    tmpdir = tempfile.mkdtemp()
+        # download artifacts
+        key = ssh_get_key(self.properties['keypriv'])
+        client = ssh_connect(self.properties['ip'], 'ubuntu', key)
+        download_directory(client, "output/", tmpdir)
+        # build webpage
+        create_html_report(tmpdir)
 
-    # download artifacts
-    key = ssh_get_key(properties['keypriv'])
-    client = ssh_connect(properties['ip'], 'ubuntu', key)
-    download_directory(client, "output/", tmpdir)
-    # build webpage
-    create_html_report(tmpdir)
+        # and upload somewhere
+        # TODO: include properies like branch/tag/commit id/PR/time consumed
+        to_delete = True
+        if os.getenv('RESULT_UPLOAD_URL'):
+            print("Uploading result to", os.getenv('RESULT_UPLOAD_URL'))
+            to_delete = upload_result(tmpdir, os.getenv('RESULT_UPLOAD_URL'))
 
-    # and upload somewhere
-    # TODO: include properies like branch/tag/commit id/PR/time consumed
-    to_delete = True
-    if os.getenv('RESULT_UPLOAD_URL'):
-        print("Uploading result to", os.getenv('RESULT_UPLOAD_URL'))
-        to_delete = upload_result(tmpdir, os.getenv('RESULT_UPLOAD_URL'))
+        # remove tmpdir
+        if to_delete:
+            shutil.rmtree(tmpdir)
+        else:
+            print("Result of build available in", tmpdir)
 
-    # remove tmpdir
-    if to_delete:
-        shutil.rmtree(tmpdir)
-    else:
-        print("Result of build available in", tmpdir)
+    def cleanup(self):
+        if self.properties.get('keycreated', False):
+            remove_keypair(create_vm.CONN, self.properties['keyname'])
 
-    if properties['keycreated']:
-        remove_keypair(create_vm.CONN, properties['keyname'])
+        if self.properties.get('server_id', None):
+            create_vm.delete_server(create_vm.CONN,
+                                    self.properties['server_id'])
+        self.properties = {}
 
-    create_vm.delete_server(create_vm.CONN, properties['server_id'])
+    def __del__(self):
+        self.cleanup()
+
 
 class ST2Job(Action):
     def __init__(self, config):
@@ -83,8 +97,26 @@ class ST2Job(Action):
 
     def run(self, **kwargs):
         create_vm.os_connect()
-        run_job()
+        job = OpenstackJob()
+        try:
+            job.run()
+        except:
+            traceback.print_exc()
+        job.cleanup()
+
+
+def cli():
+    for key in ['OS_AUTH_URL', 'OS_PASSWORD', 'OS_TENANT_ID',
+                'OS_TENANT_NAME', 'OS_USERNAME']:
+        create_vm.CREDS[key] = os.getenv(key)
+    create_vm.os_connect()
+    job = OpenstackJob()
+    try:
+        job.run()
+    except:
+        traceback.print_exc()
+    job.cleanup()
+
 
 if __name__ == "__main__":
-    create_vm.os_connect()
-    run_job()
+    cli()
