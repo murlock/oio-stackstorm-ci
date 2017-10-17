dde313b595847e86c1b2ed22388431a0b8e00e23
Michael Bonfils <murlock42@gmail.com>
2017-10-17 11:16:02 +0200

    Add building docker oio-fs image

--- actions/build.sh
+++ actions/build.sh
@@ -10,3 +10,19 @@ cd dockerfiles/openio-sds-source/
 docker build -t sds-source ubuntu
 
 # TODO: push docker image to registry ?
+
+
+
+#
+# OIOFS
+#
+if [ -d oiofs ]; then
+    rm -rf oiofs
+fi
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "No GITHUB Token, cannot interacte with private repository"
+else
+    git clone https://unused:${GITHUB_TOKEN}@github.com/openio-private/oio-fs.git
+    cd oio-fs/docker
+    docker build -t oiofs --build-arg GITHUB_TOKEN=${GITHUB_TOKEN} .
+fi
