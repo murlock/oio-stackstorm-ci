2c165f3fc8ed880f8f342dbd513a8f362529e194
Michael Bonfils <murlock42@gmail.com>
2017-10-18 16:35:27 +0200

    Split logic between openstack and test inside VM

--- actions/prepare_vm.py
+++ actions/prepare_vm.py
@@ -8,8 +8,8 @@ from common import ssh_connect, upload_file, ssh_get_key
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def do_prepare(ip, username, keystr, keypass=None,
-               token=""):
+def do_prepare_deprecated(ip, username, keystr, keypass=None,
+                          token=""):
     key = ssh_get_key(keystr, keypass)
 
     client = ssh_connect(ip, username, key)
@@ -34,5 +34,39 @@ def do_prepare(ip, username, keystr, keypass=None,
     print("build docker image")
     (stdin, stdout, stderr) = client.exec_command(
         'export GITHUB_TOKEN="%s"; ./build.sh' % token)
+    for line in stdout:
+        print(line.encode('utf-8'), end="")
+
+
+def do_prepare(ip, username, keystr, keypass=None,
+               token=""):
+    key = ssh_get_key(keystr, keypass)
+
+    client = ssh_connect(ip, username, key)
+
+    # Checkout oio-qa
+    print("XXXXXXX CHECKOUT")
+    (stdin, stdout, stderr) = client.exec_command(
+        'git clone https://github.com/murlock/oio-qa.git -b stackstorm')
+    for line in stdout:
+        print(line.encode('utf-8'), end="")
+
+    # Fill token
+    print("XXXXXXX TOKEN")
+    (stdin, stdout, stderr) = client.exec_command(
+        'echo export GITHUB_TOKEN=%s >> .profile' % token)
+    for line in stdout:
+        print(line.encode('utf-8'), end="")
+
+    # Launch install step
+    print("XXXXXXX INSTALL")
+    (stdin, stdout, stderr) = client.exec_command(
+        'cd oio-qa/prepare && ./install.sh')
+    for line in stdout:
+        print(line.encode('utf-8'), end="")
+    # Launch build step
+    print("XXXXXXX BUILD")
+    (stdin, stdout, stderr) = client.exec_command(
+        'cd oio-qa/prepare && ./build.sh')
     for line in stdout:
         print(line.encode('utf-8'), end="")
--- actions/run_vm.py
+++ actions/run_vm.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 import os
 
-from common import ssh_connect, upload_file, ssh_get_key
+from common import ssh_connect, ssh_get_key
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,9 +12,7 @@ def do_run(ip, username, keystr, keypass=None):
     key = ssh_get_key(keystr, keypass)
     client = ssh_connect(ip, username, key)
 
-    # upload minimal requirements stuff
-    upload_file(client, BASE_DIR + '/run.sh', perm=0o0555)
-
-    (stdin, stdout, stderr) = client.exec_command('./run.sh', get_pty=True)
+    (stdin, stdout, stderr) = client.exec_command(
+        'cd oio-qa/run && ./run.sh', get_pty=True)
     for line in stdout:
         print(line.encode('utf-8'), end="")
